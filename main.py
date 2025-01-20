97bba0ce878ca4ab16164f54cfca8ba8fa3cdc34
Jbamenorfe <jbamenorfe@gmail.com>
2025-01-20 04:02:04 +0000

    The US States game uses Python's turtle and pandas modules to create a game.

--- main.py
+++ main.py
@@ -28,19 +28,53 @@ states = pandas.read_csv("./50_states.csv")
 
 # Create a list of states from the state column in the states pandas object
 states_list = states.state.to_list()
-print(states_list)
+# print(states_list)
 
-game_is_on = True
 correct_answer = 0
-total_states = len(states.state)
-listed_states = []
-while game_is_on:
+total_states = 50
+correct_states = []
+correct_states_count = len(correct_states)
+
+all_missing_states = {}     # Dictionary to hold missing states' name, x-cor and y-cor
+missing_state_list = []     # List to hold missing state names
+missing_states_xcor_list = []   # List to hold missing state's x-coordinates
+missing_states_ycor_list = []   # List to hold missing state's y-coordinates
+
+
+while correct_states_count < total_states:
     # Take an input from the user
     answer_state = screen.textinput(title=f"{correct_answer}/{total_states} States Correct", prompt= "What's another state's name ?")
     answer_state = answer_state.title()
 
+    if answer_state == "Exit":
+        for state_item in states_list:
+            if not state_item in correct_states:
+                missing_state = states[states.state == state_item]  # Missing state (row object)
+
+                missing_state_item = missing_state.state.item()     # Missing state's name
+                xcor = missing_state.x.item()                       # Missing state's x-coordinate
+                ycor = missing_state.y.item()                       # Missing state's y-coordinate
+
+                missing_state_list.append(missing_state_item)       # Add missing state's name to missing state list
+                missing_states_xcor_list.append(xcor)               # Add missing state's xcor to its list
+                missing_states_ycor_list.append(ycor)               # Add missing state's ycor to its list
+
+                # Populate the dictionary of all missing states
+                all_missing_states = {
+                    "state": missing_state_list,
+                    "x": missing_states_xcor_list,
+                    "y": missing_states_ycor_list
+        }
+
+        # Convert all_missing_state dictionary to pandas dataframe
+        df = pandas.DataFrame(all_missing_states)
+
+        # Store df in a .csv file
+        df.to_csv("missing_states.csv")
+        break
+
     if answer_state in states_list:
-        if not answer_state in listed_states:                   # Ensure that a state is not repeated
+        if not answer_state in correct_states:                   # Ensure that a state is not repeated
             found_state = states[states.state == answer_state]  # Get the state name and corresponding x, y coordinates
             found_state_x = int(found_state.x.iloc[0])      # Get the state's x_coordinate and convert it to an integer using the int(ser.iloc[0]) function.
             found_state_y = int(found_state.y.iloc[0])      # Get the state's y_coordinate and convert it to an integer
@@ -48,8 +82,4 @@ while game_is_on:
             # Create a turtle object for the found state
             create_state_turtle(state_name=answer_state, x_pos=found_state_x, y_pos=found_state_y)
             correct_answer += 1
-            listed_states.append(answer_state)
-    if correct_answer == total_states:
-        screen.textinput(title="Congratulations!", prompt="You have completed the game successfully!\nPlease click on \"OK\" to close the game")
-        game_is_on = False
-screen.exitonclick()
+            correct_states.append(answer_state)
